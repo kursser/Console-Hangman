bfe6c7aed1c4aeec47861dca9393d72f442ad6e8
kursser <usa.man1337228@gmail.com>
2026-05-09 13:58:01 +0300

    Переписал в обычные методы, убрал использование из глобальной области

--- hangman/ui/get_hangman.py
+++ hangman/ui/get_hangman.py
@@ -1,9 +1,15 @@
 from ui.ui_hangman import stages
-class HangmanRender:
-    def __init__(self):
-        self._stages = {3: stages[0], 6: stages[1], 10: stages[2]}
 
-    def get(self, attempts):
-        return self._stages[attempts]
+
+def get_hangman(attempts):
+    dict_stages = {3: stages[0], 6: stages[1], 10: stages[2]}
+    return dict_stages[attempts]
+
+# class HangmanRender:
+#     def __init__(self):
+#         self._stages = {3: stages[0], 6: stages[1], 10: stages[2]}
+#
+#     def get(self, attempts):
+#         return self._stages[attempts]
 
 
